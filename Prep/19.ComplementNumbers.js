bc9bc11d680bb84cc2dae2e0e79ccb416c9d6d13
ramyatata <ramyatata007@gmail.com>
2018-03-23 23:19:46 -0700

    compliment numbers solved

--- Prep/19.ComplementNumbers.js
+++ Prep/19.ComplementNumbers.js
@@ -4,3 +4,20 @@ input - [1, -2, 6, 7, -1, 4, 2, 8]
 then result should be [1, 2]
  */
 
+function complementNumbers(arr){
+  let obj = {};
+  let results = [];
+  for(let i = 0; i < arr.length; i++){
+    if(obj[arr[i]] === undefined){
+      let compliment = (-1)*arr[i];
+      obj[compliment] = true;
+    } else {
+      if(arr[i] < 0){
+        results.push((-1)*arr[i]);
+      } else {
+        results.push(arr[i]);
+      }
+    }
+  }
+  return results;
+}
