0ca9b6262c7debcf5bff1807026a0fc492ef1587
ramyatata <ramyatata007@gmail.com>
2018-03-23 23:09:40 -0700

    ReverseVowelsOrder

---
+++ Prep/18.ReverseVowels.js
@@ -0,0 +1,26 @@
+/* Reverse Vowels
+ leave the consonents in place and swap the vowels(reverse)
+ under linear time and constant space
+ if input is ["h", "e", "i", "o", "s", "a"] output should be ["h", "a", "o", "i", "s", "e"]
+*/
+function ReverseVowels(arr) {
+  let left = 0;
+  let right = arr.length;
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  while(left < right && right > 0){
+    while(vowels.indexOf(arr[left]) === -1){
+      left++;
+    }
+    while(vowels.indexOf(arr[right]) === -1){
+      right--;
+    }
+    if(left < right){
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return arr;
+}
+SwapVowels(["h", "e", "i", "o", "s", "a"]])
